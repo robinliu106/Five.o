517a1bbfb58cd572d7eae3b23fd808b27064d215
Robin Liu <robinliu106@gmail.com>
2020-06-13 22:50:41 -0400

    edited keys

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -83,5 +83,3 @@ console.log("api", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 export default GoogleApiWrapper({
     apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
 })(MapContainer);
-
-//"AIzaSyCdxrSF2eabGtkGRtrcH3GCvFl4-N90bJ0",
