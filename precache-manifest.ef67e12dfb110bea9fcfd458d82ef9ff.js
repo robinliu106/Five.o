488599c79c71d7016302c6651fcb5979ce24b191
Robin Liu <robinliu106@gmail.com>
2021-02-06 21:58:50 -0500

    Updates

---
+++ precache-manifest.ef67e12dfb110bea9fcfd458d82ef9ff.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8065ec88fad2ae3085575dd7ad69e7",
+    "url": "/Five.o/index.html"
+  },
+  {
+    "revision": "5dc985d060b2147b7529",
+    "url": "/Five.o/static/css/2.8f4750d0.chunk.css"
+  },
+  {
+    "revision": "4692b50cf7bbd41532c0",
+    "url": "/Five.o/static/css/main.ff6a71c5.chunk.css"
+  },
+  {
+    "revision": "5dc985d060b2147b7529",
+    "url": "/Five.o/static/js/2.9730292e.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/Five.o/static/js/2.9730292e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4692b50cf7bbd41532c0",
+    "url": "/Five.o/static/js/main.050a0b37.chunk.js"
+  },
+  {
+    "revision": "cb08d1f09244c6801187",
+    "url": "/Five.o/static/js/runtime-main.bb8199ae.js"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/Five.o/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/Five.o/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/Five.o/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/Five.o/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/Five.o/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/Five.o/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/Five.o/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/Five.o/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/Five.o/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/Five.o/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/Five.o/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/Five.o/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/Five.o/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/Five.o/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/Five.o/static/media/fa-solid-900.9ae050d1.woff2"
+  }
+]);
